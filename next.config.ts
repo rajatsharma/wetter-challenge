83d687deee3d8a58dba37d246aeb53359d34eb69
Rajat Sharma <lunasunkaiser@gmail.com>
2025-05-16 06:47:49 +0530

    try experimental features

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  experimental: {
+    useLightningcss: true,
+    inlineCss: true,
+  },
   logging: {
     fetches: {
       fullUrl: true,
